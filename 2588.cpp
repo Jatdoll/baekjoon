bcbe85ebfa663bf78eff5bb8020d3bc21565bf8e
Jatdoll <jatdoll0705@gmail.com>
2024-01-23 00:13:24 +0900

    2024-1-22-study

---
+++ 2588.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int a = 472;
+    int b = 385;
+
+    //cin >> a;
+    //cin >> b;
+
+    string s = to_string(b);
+
+    int b_1 = (int)s[2] - '0';
+    int b_2 = (int)s[1] - '0';
+    int b_3 = (int)s[0] - '0';
+
+    cout<<b_1 * a <<endl;
+    cout<<b_2 * a <<endl;
+    cout<<b_3 * a <<endl;
+    cout<<a*b<<endl;
+}
